2e7cac4ee7e41f7319e9d32a88128c046853ec8d
saigonkim <studioplab@gmail.com>
2026-03-13 22:13:44 +0900

    Deploy: Migrated middleware to proxy (Next.js 16) and integrated Sentry

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  // Suppresses source map uploading logs during bundling
+  silent: true,
+  org: "the-bundle-org",
+  project: "the-bundle",
+
+  // Anr (Application Not Responding) can be enabled here if needed
+  // ... other options
+});
